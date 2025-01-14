3f667d358c151f56268618de45431be4c35a59f5
Akshay Dodkade <akshaydodkade@gmail.com>
2025-01-14 14:39:40 +0530

    leetcode - added solution for remove element

--- problem-merge-sorted-array/solution.py
+++ problem-merge-sorted-array/solution.py
@@ -1,26 +1,13 @@
-def mergeArrays(num1, num2, m, n):
-  p1, p2, p = m - 1, n - 1, m + n - 1
-
-  while p1 >= 0 and p2 >= 0:
-    if num2[p2] > num1[p1]:
-      num1[p] = num2[p2]
-      p2 -= 1
-    else:
-      num1[p] = num1[p1]
-      p1 -= 1
-    p -= 1
-
-  # sorting p2
-  while p2 >= 0:
-    num1[p] = num2[p2]
-    p2 -= 1
-    p -= 1
+def removeElement(nums, val):
+  k = 0
+  for i in range(len(nums)):
+    if nums[i] != val:
+      nums[k] = nums[i]
+      k += 1
   
-  return num1
-
+  return k
 
 if __name__ == '__main__':
-  i = list(map(int, input().split()))
-  num1 = [1, 4, 5, 0, 0, 0, 0, 0]
-  num2 = [2, 3, 4, 4, 8]
-  print(mergeArrays(num1, num2, 3, 5))
+  nums = [3, 2, 2, 3]
+  val = 3
+  print(removeElement(nums, val))
