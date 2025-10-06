6f41068d872ca20b2c8fab830eadd55ded399b69
Akshay Dodkade <akshaydodkade@gmail.com>
2025-10-06 16:23:49 +0530

    leetcode - added solution for three sum

--- problem-three-sum/solution.py
+++ problem-three-sum/solution.py
@@ -1,23 +1,45 @@
 from typing import List
 
-def productExceptSelf(nums: List[int]) -> List[int]:
-  res = [1] * (len(nums))
+class Solution:
+  # method: two pointers
+  def threeSum(self, nums: List[int]) -> List[List[int]]:
+    res = []
 
-  # do prefix calculations
-  prefix = 1
-  for i in range(len(nums)):
-    res[i] = prefix
-    prefix *= nums[i]
+    # sort nums array
+    nums.sort()
+    
+    for i, a in enumerate(nums):
+      # base case
+      if a > 0:
+        break
+
+      # skip duplicate values
+      if i > 0 and a == nums[i - 1]:
+        continue
+
+      # use 2 pointers for remaining data
+      l, r = i + 1, len(nums) - 1
+      while l < r:
+        sum = a + nums[l] + nums[r]
+        if sum > 0:
+          # sum is higher - decrease right
+          r -= 1
+        elif sum < 0:
+          # sum is lower - increase left
+          l += 1
+        else:
+          # sum is 0 so store the values
+          res.append([a, nums[l], nums[r]])
+          l += 1
+          r -= 1
+          # skip duplicate res (eg: [-2, -2, 0, 0, 2, 2])
+          while nums[l] == nums[l - 1] and l < r:
+            l += 1
+
+    return res
   
-  # do postfix calculations
-  postfix = 1
-  for i in range(len(nums) - 1, -1, -1):
-    res[i] *= postfix
-    postfix *= nums[i]
-
-  return res
-
-
-if __name__ == '__main__':
-  nums = [1, 2, 4, 6]
-  print(productExceptSelf(nums))
+if __name__ == "__main__":
+  s = Solution()
+  nums = [-1, 0, 1, 2, -1, -4]
+  # nums = [-2, -2, 0, 0, 2, 2]
+  print(s.threeSum(nums))
