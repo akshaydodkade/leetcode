ff986debad3c09baac7e154fd8f9742659e7d2f4
Akshay Dodkade <akshaydodkade@gmail.com>
2026-04-02 01:31:27 +0530

    added solution for robot collisions

--- problem-robot-collisions/solution.py
+++ problem-robot-collisions/solution.py
@@ -1,7 +1,58 @@
 class Solution:
-  def template(self, positions, healths, direction):
-    print(positions, healths, direction)
-    return 'end'
+  def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
+    n = len(positions)
+
+    # Step 1: combine and sort
+    robots = []
+    for i in range(n):
+      robots.append((positions[i], healths[i], directions[i], i))
+
+    robots.sort()  # sort by position
+
+    stack = []  # will store indices of robots (in robots array)
+
+    # Convert to mutable list
+    health = [h for _, h, _, _ in robots]
+
+    for i in range(n):
+      pos, h, d, idx = robots[i]
+      
+      if d == 'R':
+        stack.append(i)
+      else:
+        # moving left
+        while stack and health[i] > 0:
+          j = stack[-1]  # last R robot
+          
+          if health[j] < health[i]:
+            # R dies
+            stack.pop()
+            health[i] -= 1
+            health[j] = 0
+          
+          elif health[j] > health[i]:
+            # L dies
+            health[j] -= 1
+            health[i] = 0
+            break
+          
+          else:
+            # both die
+            stack.pop()
+            health[j] = 0
+            health[i] = 0
+            break
+
+    # Step 4: collect survivors
+    result = []
+    for i in range(n):
+      if health[i] > 0:
+        result.append((robots[i][3], health[i]))  # (original index fhealth)
+
+    # sort by original index
+    result.sort()
+
+    return [h for _, h in result]
 
 
 if __name__ == '__main__':
@@ -9,4 +60,4 @@ if __name__ == '__main__':
   positions = [5,4,3,2,1]
   healths = [2,17,9,15,10]
   directions = "RRRRR"
-  print(s.template(positions, healths, directions))
+  print(s.survivedRobotsHealths(positions, healths, directions))
--- template.py
+++ template.py
@@ -1,7 +1,9 @@
-def template(n):
-  return n
+class Solution:
+  def template(self, n):
+    return n
 
 
 if __name__ == '__main__':
   n = list(map(int, input().split()))
-  print(template(n))
+  s = Solution()
+  print(s.template(n))
